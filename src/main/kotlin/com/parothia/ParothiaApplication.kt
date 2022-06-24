83147d612971a917ecfc32559606edcf311240ec
parothia <anurag786paro@gmail.com>
2022-06-24 23:38:57 +0530

    Basic auth working with inmemory db

--- src/main/kotlin/com/parothia/ParothiaApplication.kt
+++ src/main/kotlin/com/parothia/ParothiaApplication.kt
@@ -1,10 +1,21 @@
 package com.parothia
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.context.annotation.Bean
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @SpringBootApplication
-class ParothiaApplication
+class ParothiaApplication : SpringBootServletInitializer() {
+    override fun configure(builder: SpringApplicationBuilder?): SpringApplicationBuilder {
+        return builder!!.sources(ParothiaApplication::class.java)
+    }
+
+    @Bean
+    fun bCryptPasswordEncoder() = BCryptPasswordEncoder()
+}
 
 fun main(args: Array<String>) {
     runApplication<ParothiaApplication>(*args)
--- src/main/kotlin/com/parothia/security/MySecurityConfig.kt
+++ src/main/kotlin/com/parothia/security/MySecurityConfig.kt
@@ -2,6 +2,8 @@ package com.parothia.security
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -16,17 +18,20 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager
 @EnableWebSecurity
 class MySecurityConfig : WebSecurityConfigurerAdapter() {
 
-//    @Autowired
-//    private lateinit var passwordConfig: PasswordConfig;
+    @Autowired
+    private lateinit var passwordEncoder: BCryptPasswordEncoder;
 
 
-    override fun configure(http: HttpSecurity?) {
+    override fun configure(http: HttpSecurity) {
         //basic auth no logout and every time u need to send username and password -- popup
-        http!!
+        http
+            .csrf().disable()
             .authorizeRequests()
             .antMatchers("/", "index", "/css/*") // patterns specified which u want to make public
             .permitAll() //allow specified patterns in antmatchers
-            .antMatchers("/api/v1/**").hasRole(UserRole.ADMIN.name)
+            .antMatchers("/api/v1/**").hasRole(UserRole.USER.name)
+            .antMatchers("**").hasRole(UserRole.ADMIN.name)
+//            .antMatchers(HttpMethod.DELETE,"/api/v1/**").hasAuthority(UserPermissions.USER_READ.name) //adding permission based on method
             .anyRequest()
             .authenticated()
             .and()
@@ -36,15 +41,32 @@ class MySecurityConfig : WebSecurityConfigurerAdapter() {
     override fun userDetailsService(): UserDetailsService { //how u retreive users from database
         val user: UserDetails = User.builder() // this is creating user and using ImMemoruDb
             .username("anurag")
-            .password(BCryptPasswordEncoder().encode("password")) //password must be encoded
+            .password(passwordEncoder.encode("password")) //password must be encoded
             .roles(UserRole.USER.name) // internally ROLE_USER
             .build()
 
         val user1: UserDetails = User.builder() // this is creating user and using ImMemoruDb
             .username("parothia")
-            .password(BCryptPasswordEncoder().encode("password")) //password must be encoded
+            .password(passwordEncoder.encode("password")) //password must be encoded
             .roles(UserRole.ADMIN.name) // internally ROLE_USER
             .build()
         return InMemoryUserDetailsManager(user, user1)
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.inMemoryAuthentication()
+            .withUser(
+                User.builder() // this is creating user and using ImMemoruDb
+                    .username("parothia")
+                    .password(passwordEncoder.encode("password")) //password must be encoded
+                    .roles(UserRole.ADMIN.name) // internally ROLE_USER
+                    .build()
+            ).withUser(
+                User.builder() // this is creating user and using ImMemoruDb
+                    .username("anurag")
+                    .password(passwordEncoder.encode("password")) //password must be encoded
+                    .roles(UserRole.USER.name) // internally ROLE_USER
+                    .build()
+            )
     }
 }
